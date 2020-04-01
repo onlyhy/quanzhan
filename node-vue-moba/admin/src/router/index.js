089269f01772671052ac065635b7bbb25e6b7147
onlyhy <hyuestu@163.com>
2020-04-01 16:52:10 +0800

    2-17 登录界面

--- node-vue-moba/admin/src/router/index.js
+++ node-vue-moba/admin/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
@@ -16,6 +17,9 @@ import AdminUserList from '../views/AdminUserList.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',name: 'login',component: Login
+  },
   {
     path: '/',
     name: 'main',
