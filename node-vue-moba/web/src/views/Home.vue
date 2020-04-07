270ca5bc53debb1aedd7cfafc4f16a4b4a8a734b
onlyhy <hyuestu@163.com>
2020-04-07 17:29:33 +0800

    3.7首页顶部轮播图片（vue swiper）

--- node-vue-moba/web/src/views/Home.vue
+++ node-vue-moba/web/src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-   首页
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/home/03bbc2de1ee95d49603faaeab5fa3a50.jpeg" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/home/3a349f0c3496eee4ec7276fe52ba5120.jpeg" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/home/718181004680072.jpg" alt />
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-2"
+      slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-
-}
+  name: "Home",
+  data() {
+    return {
+      swiperOptions:{
+         pagination: {
+            el: '.pagination-home'
+          },
+      }
+    }
+  },
+};
 </script>
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+  .pagination-home{
+    .swiper-pagination-bullet{
+      opacity: 1;
+      border-radius: 0.1538rem;
+      background:map-get($colors, 'white' );
+      &.swiper-pagination-bullet-active{
+        background: map-get($colors, 'info' );
+      }
+    }
+  }
+</style>
---
+++ node-vue-moba/web/vue.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2020-04-07 16:43:11
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-04-07 16:48:43
+ */
+module.exports = {
+    devServer:{
+        port:5555
+    }
+}
